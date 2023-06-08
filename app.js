251b1d4c3a4793f165b27f34d9afb9000f59dc1e
Jakob <shavinski.jakob@gmail.com>
2023-06-08 10:34:09 -0700

    add cors

--- app.js
+++ app.js
@@ -3,12 +3,17 @@
 /** Express app for jobly. */
 
 const express = require("express");
+const cors = require("cors");
 
 const photosRoutes = require("./routes/photos");
 const uploadRoutes = require("./routes/upload");
 
 const app = express();
 
+// TODO: When deployed switch the origin url 
+app.use(cors({
+    origin: 'http://localhost:3000'
+  }));
 app.use(express.json());
 
 app.use("/photos", photosRoutes);
